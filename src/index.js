89ca4d89f8587357eb5d5d179043529066b60cb0
Khanh Lu Tran <khanhlu2013@gmail.com>
2019-08-12 21:49:18 -0700

    create index.js as an entry point to react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+let div = document.createElement("div");
+div.setAttribute("id", "app");
+document.body.appendChild(div);
+
+ReactDOM.render(
+  <div>
+    <h5>zillow assignment</h5>
+  </div>,
+  document.getElementById("app")
+);
